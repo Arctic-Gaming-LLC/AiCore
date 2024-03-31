45409efc96fe8d2aaddb45c89aa42ef4d48711b2
Arctic <arcticdev@arcticgaming.dev>
2024-03-31 12:45:28 -0400

    Changed openServices to a List instead of a Set.
    Bumped ver 1.2.0

--- src/main/java/dev/arctic/aicore/AiCore.java
+++ src/main/java/dev/arctic/aicore/AiCore.java
@@ -5,23 +5,20 @@ import dev.arctic.aicore.utils.CommandManager;
 import dev.arctic.aicore.utils.TabComplete;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import java.util.*;
 
 public final class AiCore extends JavaPlugin {
 
     public static AiCore plugin;
     /**
-     * A set of all open services, iterable for updating, viewing, and closing services
+     * A List of all open services, iterable for updating, viewing, and closing services
      */
-    public static Set<AiCoreService> openServices;
+    public static List<AiCoreService> openServices = new ArrayList<>();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         plugin = this;
-        openServices = new HashSet<>();
 
         // Register the CommandManager and TabComplete classes
         Objects.requireNonNull(this.getCommand("aicore")).setExecutor(new CommandManager());
